90d8934e92abee92382a5f69e89090a7cc90c3c2
KwekuQ <kwekuq@theapplab.co.za>
2020-07-02 21:21:42 +0200

    test(backend): modifying unit tests and adding mock

--- myweatherappapi/tests.py
+++ myweatherappapi/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from faker import Faker
 from rest_framework.test import APIRequestFactory
+from unittest.mock import MagicMock
+from .services.rest import Rest
 
 fake = Faker()
 
@@ -11,20 +13,22 @@ class GetWeatherTest(TestCase):
         from .views import GetWeather
         self.GetWeather = GetWeather()
 
-    def testNoCityShouldReturn400(self):
-        request = self.factory.get('/api/v1/weather', {'date': fake.date()}, format='json')
+    def testNoLatShouldReturn400(self):
+        request = self.factory.get('/api/v1/weather', {'lat': fake.random_number()}, format='json')
         response = self.GetWeather.as_view()(request)
 
         self.assertEqual(response.status_code, 400)
 
-    def testNoDateShouldReturn400(self):
-        request = self.factory.get('/api/v1/weather', {'city': fake.name()}, format='json')
+    def testNoLngShouldReturn400(self):
+        request = self.factory.get('/api/v1/weather', {'lng': fake.random_number()}, format='json')
         response = self.GetWeather.as_view()(request)
 
         self.assertEqual(response.status_code, 400)
 
     def testShouldReturn200(self):
-        request = self.factory.get('/api/v1/weather', {'city': fake.name(), 'date': fake.date()}, format='json')
+        rest = Rest()
+        rest.get_weather = MagicMock(return_value=[])
+        request = self.factory.get('/api/v1/weather', {'lat': fake.random_number(), 'lng': fake.random_number()}, format='json')
         response = self.GetWeather.as_view()(request)
 
         self.assertEqual(response.status_code, 200)
